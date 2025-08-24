ef39f0a26469a13563f79e7d131e33b1194200df
T1mothy Nj4u <93652350+TimothyNjau@users.noreply.github.com>
2025-08-24 23:15:38 +0300

    feat: Refactored the next.config file to integrate with next-pwa

--- alx-movie-app/next.config.ts
+++ alx-movie-app/next.config.ts
@@ -1,12 +1,18 @@
-import type { NextConfig } from "next";
+import withPWAInit from "@ducanh2912/next-pwa";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+
+const withPWA = withPWAInit({
+  dest: 'public'
+})
+
+const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['m.media-amazon.com'],
   },
 };
 
-module.exports = nextConfig;
-export default nextConfig;
+export default withPWA({
+  ...nextConfig
+})
